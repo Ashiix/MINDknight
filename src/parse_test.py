d969d262b9ff5e09525fc8e9777f2f6156cd88db
Ethan Friedman <etfriedman@packer.edu>
2020-07-01 16:48:53 -0400

    added ability to be able to get last time from either player.log or export file (function). Going to be useful when we need to see what data we have already given the model.

--- src/parse_test.py
+++ src/parse_test.py
@@ -4,6 +4,7 @@ import re
 import sys
 import pathlib
 import ast
+from datetime import datetime
 
 os = sys.platform
 
@@ -19,17 +20,43 @@ else:
 if not pathlib.Path(log_path).exists():
     sys.exit("The file can't be found in the given log path. This is most likely because you entered your home dir incorrectly.")
 
+cur_time = str(datetime.now()).split()[1].split(".")[0] # gets current time in same format as player.log timestamp
+print(cur_time)
+
+def get_time(type):
+    with open('test_data.txt', 'r') as export_log:
+        for findline in export_log:
+            pass
+    #player.log file
+    if type == 'log':
+        last_export_time = findline.split()[1][:-1] # gets most recent log time, removes extra chars etc.
+        print(last_export_time)
+    #export file
+    elif type == 'e_log':
+        last_export_time = findline.split()[0][:-1]
+        print(last_export_time)
+
+    else: print("use log or e_log depending on where you want to read time from")
+
+
 with open(log_path, 'r') as log:
     for line in enumerate(log):
         line = line[1].split()
         try:
             with open("test_data.txt",'a') as f:
+                #print("im here")
                 if len(line[1]) == 9 and not line[1][2] == ".":
                     time = line[1]
                     cur_dict = ast.literal_eval(line[-1:][0].split(":",1)[1].replace("false","False").replace("true","True"))
                     if cur_dict["Type"] == 307 or cur_dict["Type"] == 311 or cur_dict["Type"] == 204:
-                        print("printing line...")
+                        #print("printing line...")
                         f.write("{} {}\n".format(time, cur_dict))
         except: pass
-f.close()
-log.close()
+
+get_time('e_log')
+
+#when reading data from exported file (after exporting specific
+#data to that file) use old_time to get
+#the last time data was exported, use this
+#when giving data to model (or putting in CSV)
+#in order to not repeat data, we could also check if certain times have been exported?
