cb678621dcd7bda259a3e3d7831b9d96ff9aaa68
Ethan Friedman <etfriedman@packer.edu>
2020-07-02 16:18:42 -0400

    changed packet type

--- src/csv.py
+++ src/csv.py
@@ -1,1 +1,46 @@
-# temp_player.log
+
+
+2020.06.29 18:10:23: Received ChatMessageReceive packet:{"Type":205,"Message":"what a clown","Slot":2}
+2020.06.29 18:10:23: Received ChatUpdate packet:{"Type":405,"Slot":1,"Typing":true}
+2020.06.29 18:10:24: Received KeepAlive packet:{"Type":-1}
+2020.06.29 18:10:24: Sending Packet:{"Type":-1}
+2020.06.29 18:10:24: Sending SendTypingPacket:{"Type":404,"Typing":false}
+2020.06.29 18:10:24: Sending SendChatPacket:{"Type":204,"Message":"honk honk"}
+2020.06.29 18:10:24: Received ChatUpdate packet:{"Type":405,"Slot":1,"Typing":false}
+2020.06.29 18:10:24: Received ChatMessageReceive packet:{"Type":205,"Message":"honk honk","Slot":1}
+2020.06.29 18:10:25: Received ChatUpdate packet:{"Type":405,"Slot":0,"Typing":false}
+2020.06.29 18:10:25: Received ChatMessageReceive packet:{"Type":205,"Message":"why did hanna and cassy acc that???","Slot":0}
+2020.06.29 18:10:26: Received ServerInfo packet:{"Type":2,"PlayersOnline":39,"PlayersPlaying":29,"PublicCustomGames":4,"PlayersSearching":3,"MaintenanceIn":-1}
+2020.06.29 18:10:26: Received SelectUpdate packet:{"Type":305,"Slots":[3,4],"Submit":false,"Pass":false}
+2020.06.29 18:10:27: Received SelectUpdate packet:{"Type":305,"Slots":[2,3,4],"Submit":false,"Pass":false}
+2020.06.29 18:10:27: Received ChatUpdate packet:{"Type":405,"Slot":4,"Typing":true}
+2020.06.29 18:10:28: Received SelectPhaseEnd packet:{"Type":304,"Proposer":3,"SelectedTeam":[2,3,4],"Passed":false}
+2020.06.29 18:10:28: Received VotePhaseStart packet:{"Type":306,"Proposer":3,"Players":[2,3,4],"Duration":60000}
+2020.06.29 18:10:28: PlayState: Entering VotePhase
+2020.06.29 18:10:29: Received ChatUpdate packet:{"Type":405,"Slot":2,"Typing":true}
+2020.06.29 18:10:29: Sending SendTypingPacket:{"Type":404,"Typing":true}
+2020.06.29 18:10:29: Received ChatUpdate packet:{"Type":405,"Slot":1,"Typing":true}
+2020.06.29 18:10:30: Received VoteMade packet:{"Type":309,"Slot":3}
+
+16:47:16: {'Type': 204, 'Message': 'sup'}
+16:47:42: {'Type': 307, 'VotesFor': [0, 4], 'VotesAgainst': [3, 1, 2], 'Passed': False}
+16:48:09: {'Type': 307, 'VotesFor': [4, 1], 'VotesAgainst': [2, 0, 3], 'Passed': False}
+16:48:26: {'Type': 307, 'VotesFor': [3], 'VotesAgainst': [2, 0, 4, 1], 'Passed': False}
+16:48:39: {'Type': 307, 'VotesFor': [3, 4], 'VotesAgainst': [0, 1, 2], 'Passed': False}
+16:48:50: {'Type': 307, 'VotesFor': [3, 2, 4, 0], 'VotesAgainst': [1], 'Passed': True}
+16:49:00: {'Type': 311, 'Mission': 1, 'Failed': False, 'NumHacks': 0, 'Proposer': 4}
+16:49:27: {'Type': 204, 'Message': 'tyty'}
+16:49:32: {'Type': 204, 'Message': 'ew'}
+16:50:08: {'Type': 307, 'VotesFor': [1, 3], 'VotesAgainst': [0, 4, 2], 'Passed': False}
+16:50:44: {'Type': 307, 'VotesFor': [4, 3], 'VotesAgainst': [0, 1, 2], 'Passed': False}
+16:50:59: {'Type': 204, 'Message': 'incorrect'}
+16:51:20: {'Type': 307, 'VotesFor': [3], 'VotesAgainst': [0, 2, 4, 1], 'Passed': False}
+16:51:29: {'Type': 204, 'Message': 'really?'}
+16:51:44: {'Type': 307, 'VotesFor': [4], 'VotesAgainst': [3, 2, 0, 1], 'Passed': False}
+16:51:46: {'Type': 204, 'Message': 'ah'}
+16:52:03: {'Type': 307, 'VotesFor': [0, 4, 3, 2], 'VotesAgainst': [1], 'Passed': True}
+16:52:06: {'Type': 204, 'Message': 'hmm'}
+16:52:10: {'Type': 311, 'Mission': 2, 'Failed': True, 'NumHacks': 1, 'Proposer': 0}
+16:53:15: {'Type': 204, 'Message': 'panic'}
+16:53:29: {'Type': 204, 'Message': 'interesting'}
+16:53:34: {'Type': 204, 'Message': 'reject'}
