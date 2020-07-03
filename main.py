fbdcfe9601d4bf5869b593571d3cf6a5c95dde21
Ashiix <Ashiix@protonmail.ch>
2020-07-02 21:25:55 -0400

    Small change to prettify main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 from src.parse import Parse
-parse = Parse()
 
-parse.read_log()
+def main():
+    parse = Parse()
+
+    parse.read_log()
+
+if __name__ == "__main__":
+    main()
