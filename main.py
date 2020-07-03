cc9e473f6ee13b4a3bfeec1fbdf18346d67041fd
Ashiix <Ashiix@protonmail.ch>
2020-07-02 21:32:39 -0400

    Giving credit where credit is due

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+# MINDknight - an AI (helper) for MINDNIGHT
+# Developed by Ashiix and etfriedman
+# Original concept by Nik-Novak and Ashiix
+
 from src.parse import Parse
 
 def main():
     parse = Parse()
-
     parse.read_log()
 
 if __name__ == "__main__":
