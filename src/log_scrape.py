6ca9f5eec86a6a05083439c661c8eb5bf18d086d
Ashiix <Ashiix@protonmail.ch>
2020-07-07 14:44:42 -0400

    Updated games

--- src/log_scrape.py
+++ src/log_scrape.py
@@ -3,32 +3,35 @@
 import sys
 import os.path
 
-home = "ash"
+class Scraping:
+    def __init__(self):
+        home = "ash"
 
-log_path = "/home/{}/.config/unity3d/Nomoon/Mindnight/Player.log".format(home)
-print(log_path)
-line_num = 0
-in_game = False
-game = []
+        self.log_path = "rawgames.csv"
+        print(self.log_path)
+        self.game = []
 
-game_num = 1
-while True:
-    if not os.path.isfile('games/game{}.txt'.format(game_num)):
-        break
-    game_num += 1
+        self.game_num = 1
+        while True:
+            if not os.path.isfile('src/games/game{}.txt'.format(self.game_num)):
+                break
+            self.game_num += 1
 
-with open(log_path, 'r') as log:
-    for line in enumerate(log):
-        line_txt = line[1].replace("\n", "")
-        if "GameStart" in line_txt or in_game == True:
-            in_game = True
-            game.append(line_txt)
-            if "Received GameEnd" in line_txt:
-                with open("games/game{}.txt".format(str(game_num)), "a") as game_file:
-                    for line_game in enumerate(game):
-                        game_file.write(line_game[1]+"\n")
-                print("Wrote game to file.")
-                game_num += 1
-                in_game = False
-                game = []
-        line_num += 1
+    def external_scrape(self):
+        in_game = False
+        line_num = 0
+        with open(self.log_path, 'r') as log:
+            for line in enumerate(log):
+                line_txt = line[1].replace("\n", "")
+                if "GameStart" in line_txt or in_game == True:
+                    in_game = True
+                    self.game.append(line_txt)
+                    if "Received GameEnd" in line_txt:
+                        with open("src/games/game{}.txt".format(str(self.game_num)), "a") as game_file:
+                            for line_game in enumerate(self.game):
+                                game_file.write(line_game[1]+"\n")
+                        print("Wrote game to file.")
+                        self.game_num += 1
+                        in_game = False
+                        self.game = []
+                line_num += 1
